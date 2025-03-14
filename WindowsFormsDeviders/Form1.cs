2e5d6a80e4b28dbfb9f0199c0091e59788124f83
zirael <randrrro@gmail.com>
2025-03-14 22:20:15 +0300

    Update Form1.cs
    ААААААА

--- WindowsFormsDeviders/Form1.cs
+++ WindowsFormsDeviders/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,16 @@ namespace WindowsFormsDeviders
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
